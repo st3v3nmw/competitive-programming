b889a13afb7d4ccd3327aee9ba7926f82c355816
Stephen Mwangi <st3v3n.mw@gmail.com>
2020-08-03 20:08:17 +0300

    problem 1B solution has a problem :P

---
+++ codeforces/0000-0099/01B_spreadsheets.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// god, that was frustrating
+int main() {
+    int n, col, row;
+    cin >> n;
+    string coord;
+    for (int i = 0; i < n; i++) {
+        cin >> coord;
+        if (coord[0] == 'R' && isdigit(coord[1]) && coord.find('C') != -1) {
+            vector<char> result;
+            for (int i = 0; i < coord.length(); i++) {
+                if (coord[i] == 'R')
+                    row = atoi(&coord[i + 1]);
+                else if (coord[i] == 'C') {
+                    col = atoi(&coord[i + 1]);
+                    break;
+                }
+            }
+            bool d = false;
+            do {
+                if (col % 26 == 0) {
+                    if (d)
+                        result.push_back('Y');
+                    else
+                        result.push_back('Z');
+                    d = true;
+                } else if (d) {
+                    d = false;
+                    if (col % 26 != 1)
+                        result.push_back(char(col % 26 + 63));
+                } else
+                    result.push_back(char(col % 26 + 64));
+                col /= 26;
+            } while (col != 0);
+
+            int s = result.size() - 1;
+            for (int i = 0; i <= s; i++)
+                cout << result[s - i];
+            cout << row << endl;
+            
+        } else {
+            col = int(coord[0]) - 64;
+            for (int i = 1; i < coord.length(); i++) {
+                if (isalpha(coord[i])) {
+                    col =  col * 26 + int(coord[i]) - 64;
+                } else {
+                    row = atoi(&coord[i]);
+                    break;
+                }
+            }
+            cout << "R" << row << "C" << col << endl;
+        }
+    }
+    return 0;
+}
