b0f4f960c2b5f581f6546f97e8f5369990fbacb9
Stephen Mwangi <st3v3n.mw@gmail.com>
2020-08-06 12:19:19 +0300

    05_Statements

--- c-compiler/compiler.cpp
+++ c-compiler/compiler.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <memory>
+#include <cstring>
+
+#define IDENT_LIM 256 // limit on identifier length
+#define IDENT_BUFFER_LEN IDENT_LIM + 1
 
 using namespace std;
 
@@ -9,7 +13,7 @@ string tokstr[] = {"+", "-", "*", "/", "intlit"};
 
 // Tokens
 enum {
-  T_EOF, T_PLUS, T_MINUS, T_STAR, T_SLASH, T_INTLIT
+  T_EOF, T_PLUS, T_MINUS, T_STAR, T_SLASH, T_INTLIT, T_PRINT, T_SEMI
 };
 
 // operator precedence for each token
@@ -91,7 +95,7 @@ class Compiler {
     public:
         Compiler(string path) {
             // read file
-            Compiler::path = path;
+            inputPath = path;
             ifstream inputFl(path, ios::in);
             if (!inputFl) {
                 cerr << "Unable to open " << path << endl;
@@ -102,34 +106,73 @@ class Compiler {
             // get the cleaned up filename
             int name_start = path.find_last_of('/');
             int name_end = path.find_last_of('.');
-            Compiler::filename = path.substr(name_start != -1 ? name_start + 1: 0, name_end != -1 ? name_end : path.length());
+            filename = path.substr(name_start != -1 ? name_start + 1: 0, name_end != -1 ? name_end : path.length());
 
-            // parse and generate assembly code
-            n_line = 1, line_pos = 0;
-            token = Token();
-            scanChars();
-            root = parse(0);
-        }
-
-        // return a tokenType's precedence
-        int opPrecedence(int tokenType) {
-            int prec = opPrec[tokenType];
-            if (prec == 0) {
-                printf("Syntax error on line %d:%d, token %d\n", n_line, line_pos, tokenType);
+            // assembly output file
+            outputPath = "./out/" + filename + ".s";
+            ofstream outputFl(outputPath, ios::out);
+            if (!outputFl) {
+                cerr << "Couldn't create out file" << endl;
                 exit(1);
             }
-            return prec;
+            outputFile.swap(outputFl);
+
+            // output preamble
+            asm_preamble();
+
+            // parse and generate assembly code
+            token = Token();
+            scanChars();
+            parse();
+            cout << filename << " parsed successfully :)" << endl;
+
+            // output postamble
+            asm_postamble();
+        }
+
+
+        /* scanning and parsing */
+    
+        void parse() {
+            int reg;
+            while (1) {
+                // match a 'print' as the first token
+                match(T_PRINT, "print");
+
+                // parse the expression & generate the assembly code
+                root = binexpr(0);
+                reg = genASM(root);
+                asm_printint(reg);
+                free_all_registers();
+                
+                // match the following semicolon
+                // stop of we're at EOF
+                match(T_SEMI, ";");
+                if (token.getToken() == T_EOF)
+                    return;
+            }
+        }
+
+        // ensure that the current token is t and fetch the next token
+        // else throw an error
+        void match(int t, string what) {
+            if (token.getToken() == t) {
+                scanChars();
+            } else {
+                cout << what << " expected on line " << n_line << endl;
+                error_exit();
+            }
         }
 
         // return an AST tree whose root is a binary operator
         // Parameter ptp is the previous token's precedence
-        shared_ptr<ASTNode> parse(int ptp) {
+        shared_ptr<ASTNode> binexpr(int ptp) {
             shared_ptr<ASTNode> left, right;
             left = primary(); // build the left sub-tree
 
             int tokenType = token.getToken();
             // base case, no tokens left
-            if (tokenType == T_EOF)
+            if (tokenType == T_SEMI) // statements terminated by a semicolon
                 return left;
 
             // while the precedence of this token
@@ -139,14 +182,14 @@ class Compiler {
 
                 // recursively call parse with the precedence
                 // of our token to build a sub-tree
-                right = parse(opPrec[tokenType]);
+                right = binexpr(opPrec[tokenType]);
 
                 // join the right sub-tree above with the tree we already have
                 shared_ptr<ASTNode> left2(new ASTNode(arithOp(tokenType), 0, left, right));
                 left = left2;
 
                 tokenType = token.getToken();
-                if (tokenType == T_EOF)
+                if (tokenType == T_SEMI) // statements terminated by a semicolon
                     break;
             }
 
@@ -168,23 +211,8 @@ class Compiler {
             }
         }
 
-        void generateAssembly() {
-            // assembly output file
-            ofstream outputFl("./out/" + filename + ".asm", ios::out);
-            if (!outputFl) {
-                cerr << "Couldn't create out file" << endl;
-                exit(1);
-            }
-            outputFile.swap(outputFl);
-
-            asm_preamble();
-            int reg = genASM(root);
-            asm_printint(reg);
-            asm_postamble();
-        }
-
         int scanChars() {
-            if (!next()) { // read the next character
+            if (!skip()) { // read the next character, skip whitespace
                 token.setToken(T_EOF);
                 return 0; // EOF
             }
@@ -202,19 +230,37 @@ class Compiler {
                 case '/':
                     token.setToken(T_SLASH);
                     break;
+                case ';':
+                    token.setToken(T_SEMI);
+                    break;
                 default:
                     if (isdigit(c)) {
                         token.setValue(scanint()); // if it's a digit, scan the literal integer value
                         token.setToken(T_INTLIT);
                         break;
+                    } else if (isalpha(c) || '_' == c) {
+                        // scan keyword or identifier
+                        scanident();
+
+                        // if it's a recognized keyword, return token
+                        int tokenType = keyword();
+                        if (tokenType) {
+                            token.setToken(tokenType);
+                            break;
+                        }
+
+                        // not a recognized keyword
+                        printf("Unrecognized symbol %s on line %d\n", buf, n_line);
+                        error_exit();
                     }
 
                     printf("Unrecognized character %c on line %d:%d\n", c, n_line, line_pos);
-                    exit(1);
+                    error_exit();
             }
             return 1; // we found a token
         }
 
+        // scan an integer
         int scanint() {
             int val = 0;
             while(isdigit(c)) {
@@ -227,16 +273,59 @@ class Compiler {
             return val;
         }
 
-        int next() { // return the next character
+        // scan an identifier
+        int scanident() {
+            int i = 0;
+            // allow alphanumeric & underscores
+            while (isalnum(c) || '_' == c) {
+                // error if we reach the identifier length limit,
+                // else append to buf and get next char
+                if (ident_lim - 1 == i) {
+                    printf("Identifier too long on line %d\n", n_line);
+                    error_exit();
+                };
+                buf[i++] = c;
+                next();
+            }
+
+            buf[i] = '\0'; // null terminate buf
+            // put back a non-valid char
+            inputFile.putback(c);
+            line_pos--;
+            return i; // length of identifier
+        }
+
+        // given a word in the buffer buf, return the matching keyword
+        // token number or 0 if it's not a keyword
+        int keyword() {
+            switch(buf[0]) {
+                case 'p':
+                    if (!strcmp(buf, "print"))
+                        return T_PRINT;
+                    break;
+            }
+            return 0;
+        }
+
+        // get the next character
+        int next() {
+            if (!(inputFile >> noskipws >> c))
+                    return 0; // EOF
+            if (c == '\n') {
+                n_line++;
+                line_pos = 0; // reset out position on the line
+            }
+
+            line_pos++; // increment our position on the current line
+            return 1; // we found a char
+        }
+
+        // skip whitespace
+        int skip() {
             c = ' ';
-            while (' ' == c || '\t' == c || '\n' == c || '\r' == c || '\f' == c) { // skip whitespace
-                if (c == '\n') {
-                    n_line++;
-                    line_pos = 0; // reset our position on the line
-                }
-                if (!(inputFile >> noskipws >> c))
-                    return 0;
-                line_pos++; // increment our position on the current line
+            while (' ' == c || '\t' == c || '\n' == c || '\r' == c || '\f' == c) {
+                if(!next())
+                    return 0; // EOF
             }
             return 1;
         }
@@ -254,24 +343,18 @@ class Compiler {
             }
         }
 
-        void traverseAST() {
-            root->traverse();
-            cout << endl;
+        // return a tokenType's precedence
+        int opPrecedence(int tokenType) {
+            int prec = opPrec[tokenType];
+            if (prec == 0) {
+                printf("Syntax error on line %d:%d, token %d\n", n_line, line_pos, tokenType);
+                error_exit();
+            }
+            return prec;
         }
-    
-    private:
-        // parsing & scanning stuff
-        char c;
-        int n_line, line_pos;
-        ifstream inputFile;
-        string path, filename;
-        shared_ptr<ASTNode> root;
-        Token token;
 
-        // code generation stuff
-        ofstream outputFile;
-        int freereg[4] = {1, 1, 1, 1};
-        string reglist[4] = {"%r8", "%r9", "%r10", "%r11"};
+
+        /* code generation */
 
         // given an AST, generate assembly code recursively
         int genASM(shared_ptr<ASTNode> node) {
@@ -294,7 +377,6 @@ class Compiler {
             }
         }
 
-
         // allocate a ree register
         int alloc_register() {
             for (int i = 0; i < 4; i++) {
@@ -307,14 +389,18 @@ class Compiler {
             exit(1);
         }
 
-
         // free a register
         void free_register(int reg) {
             if (freereg[reg] != 0) {
                 cerr << "Error trying to free register " << reg << endl;
-                exit(1);
+                error_exit();
             }
             freereg[reg] = 1;
+        }
+
+        // free all registers
+        void free_all_registers() {
+            freereg[0] = freereg[1] = freereg[2] = freereg[3] = 1;
         }
 
         // Print out the assembly preamble
@@ -363,13 +449,13 @@ class Compiler {
         }
 
         int asm_mul(int r1, int r2) {
-            outputFile << "\tmulq\t" << reglist[r1] << ", " << reglist[r2] << "\n";
+            outputFile << "\timulq\t" << reglist[r1] << ", " << reglist[r2] << "\n";
             free_register(r1);
             return r2;
         }
 
         int asm_div(int r1, int r2) {
-            outputFile << "\tmovq\t" << reglist[r1] << "%rax\n";
+            outputFile << "\tmovq\t" << reglist[r1] << ", %rax\n";
             outputFile << "\tcqo\n";
             outputFile << "\tidivq\t" << reglist[r2] << "\n";
             outputFile << "\tmovq\t%rax, " << reglist[r1] << "\n";
@@ -389,6 +475,43 @@ class Compiler {
             outputFile << "\tcall\tprintint\n";
             free_register(r);
         }
+
+
+        /* debugging */
+
+        void traverseAST() {
+            root->traverse();
+            cout << endl;
+        }
+
+        inline void error_exit() {
+            // delete created ASM output file
+            int len = outputPath.length();
+            char outputPath2[len + 1];
+            for (int i = 0; i < len; i++) {
+                outputPath2[i] = outputPath[i];
+            }
+
+            outputPath2[len] = '\0';
+            remove(outputPath2);
+            exit(1);
+        }
+    
+    private:
+        // parsing & scanning stuff
+        char c;
+        int n_line = 1, line_pos = 0, ident_lim = IDENT_LIM;
+        ifstream inputFile;
+        string inputPath, filename;
+        shared_ptr<ASTNode> root;
+        Token token;
+        char buf[IDENT_BUFFER_LEN];
+
+        // code generation stuff
+        ofstream outputFile;
+        string outputPath;
+        int freereg[4] = {1, 1, 1, 1};
+        string reglist[4] = {"%r8", "%r9", "%r10", "%r11"};
 };
 
 int main(int argc, char** argv) {
@@ -398,8 +521,8 @@ int main(int argc, char** argv) {
     }
 
     Compiler compiler(argv[1]);
-    compiler.traverseAST();
-    compiler.generateAssembly();
+    // compiler.traverseAST();
+    // compiler.generateAssembly();
 
     return 0;
 }
